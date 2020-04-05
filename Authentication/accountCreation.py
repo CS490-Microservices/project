b3b6085e3a2938845dd4293261ba2b1e1a185245
NMerz <nmerz@icloud.com>
2020-04-05 17:19:30 -0400

    Update auth for VPC usage

--- Authentication/accountCreation.py
+++ Authentication/accountCreation.py
@@ -1,19 +1,23 @@
 import json
 import boto3
+import botocore
 import pymysql
 import sys
 from passlib.hash import sha256_crypt
 import passlib
 
-s3_connector = boto3.resource('s3')
+s3_connector = boto3.resource('s3', 'us-east-2', config=botocore.config.Config(s3={'addressing_style':'path'}))
 
 
 def lambda_handler(event, context):
-    dbUser = "Lambda"
-    dbpass = s3_connector.Object("config", "dbpass.txt").get()['Body'].read().decode('utf-8')
-    dbaddress = s3_connector.Object("config", "dbaddress.txt").get()['Body'].read().decode('utf-8')
-    dbname = s3_connector.Object("config", "dbname.txt").get()['Body'].read().decode('utf-8')
-    databaseConnection = pymysql.connect(dbaddress, dbUser, dbpass, dbname)
+    dbuser = "Lambda"
+    print(dbuser)
+    dbpass = s3_connector.Object("imageserviceauthconfigs", "dbpass.txt").get()['Body'].read().decode('utf-8')
+    dbaddress = s3_connector.Object("imageserviceauthconfigs", "dbaddress.txt").get()['Body'].read().decode('utf-8')
+    dbname = s3_connector.Object("imageserviceauthconfigs", "dbname.txt").get()['Body'].read().decode('utf-8')
+    print(dbaddress)
+    databaseConnection = pymysql.connect(dbaddress, dbuser, dbpass, dbname)
+    print(databaseConnection)
     cursor = databaseConnection.cursor()
     cursor.execute("SELECT hash FROM authtable WHERE username=%s;", (event["user"],))
     if len(cursor.fetchall()) != 0:
